fe7670f27590eea6f43f5fb560db91164046d668
Mayur <www.mpawar1034567@gmail.com>
2019-01-29 21:31:43 +0530

    Initial Commit

---
+++ Variables & Datatypes.py
@@ -0,0 +1,7 @@
+character_name = "X"
+character_age = "Y"
+
+print("There once was a man named " + character_name)
+print("He was " + str(character_age) + " years old")
+print("He really liked the name " + character_name)
+print("But didn't like being " + str(character_age))
